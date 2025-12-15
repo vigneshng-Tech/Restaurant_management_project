887ac177eac6d82f82833734f67e8507be2729d4
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-15 15:26:31 +0530

    63843

--- home/views.py
+++ home/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import MenuCategorySerializer
+
+
+class MenuCategoryListView(ListAPIView):
+    class Meta:
+        model = MenuCategory
+        fields = ['name']
+
+from rest_framework.generics import ListAPIView
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
+
+class MenuCategoryListView(ListAPIView):
+    queryset = MenuCategory.objects.all()
+    serilaizer_class = MenuCategorySerializer
+
+
+from django.urls import path
+from .views import MenuCategoryListView
+
+urlpatterns = [
+    path('menu-Catergories/', MenuCategoryListView.as_view(), name='menu-categories'),
+]
