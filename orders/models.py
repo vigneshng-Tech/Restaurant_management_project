37be5269840b9bd1ded00e66553edb8a2730b342
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-18 22:30:52 +0530

    64082

--- orders/models.py
+++ orders/models.py
@@ -42,3 +42,14 @@ class order(models.Model)
 
     def __str__(self):
         return f"order {self.id}"
+
+from django.contrib.auth.models import User
+
+class order(models.Model)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"order {self.id}"
