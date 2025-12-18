b53c7cabb23d3977693f9e299dbaefe6fe26510a
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-18 21:22:54 +0530

    64083

--- home/models.py
+++ home/models.py
@@ -9,3 +9,11 @@ class MenuCategory(models.Model):
     def __str__(self):
         return self.name
         
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.CASCADE)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
