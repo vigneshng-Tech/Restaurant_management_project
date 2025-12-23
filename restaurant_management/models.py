021835392dbc1886281a8c1bbe89e47888ab7b02
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-23 14:35:39 +0530

    64572

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -8,3 +8,17 @@ class Restaurant(models.Model):
     
     def __str__(self):
         return self.name
+
+class NutritionalInformation(models.Model):
+
+    menu_item = models.ForeignKey(
+        'MenuoItem'
+        on_delete=models.CASCADE
+    )
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5, decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item} - {self.calories}kcal"
