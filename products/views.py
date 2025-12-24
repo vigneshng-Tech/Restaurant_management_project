0510d54a25815016f0744069d2ea645a19cf9617
Vignesh Gunagi <gunagivignesh96@gmail.com>
2025-12-24 13:58:35 +0530

    64571

--- products/views.py
+++ products/views.py
@@ -24,3 +24,17 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+from rest_framework.generics import ListApiView
+from .models import Item
+from .serializer import ItemSerializer
+
+class ItemSearchView(ListApiView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        search = self.request.query_params.get('search')
+        if search:
+            return Item.objects.filter(name_icontains=search)
+        return Item.objects.all()
+
