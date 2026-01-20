2fe8bb179b645350694deb7a68c2a428e736dc8b
Vignesh Gunagi <gunagivignesh96@gmail.com>
2026-01-20 23:24:25 +0530

    64773

--- home/serializers.py
+++ home/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from.models import Ingredient
+from.models import MenuCategory, Ingredient
 
 class IngredientSerializer(serializers.Modelserializer):
     class Meta:
+        model = Ingredient
+        fields =['id', 'name']
+
+
+class IngredientSerializer(serializers.Modelserializer):
+    class MetaL:
         model = Ingredient
         fields =['id', 'name']
         
