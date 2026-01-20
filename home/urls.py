3a5aa2d2c5ec379fa3ea3c9f084df81e8482e672
Vignesh Gunagi <gunagivignesh96@gmail.com>
2026-01-20 23:28:04 +0530

    64773

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
-from .views import MenuItemIngredientsView
+from django.urls import path
+from .views import MenuICategoryListView, MenuItemIngredientsView
 
-urlpatterns += [
+
+urlpatterns = [
+    path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
+    
     path(
         'api/menu-items/<int:pk>/ingredients/',
         MenuItemIngredientsView.as_view().
