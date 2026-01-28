a5dddb23eb62c637081bf04a561bb28e2f549f45
Vignesh Gunagi <gunagivignesh96@gmail.com>
2026-01-28 17:51:15 +0530

    64775

--- home/models.py
+++ home/models.py
@@ -21,9 +21,18 @@ class MenuItem(models.Model):
 class DailySpecial(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField()
+
+    objects = DailySpecialManager()
 
     def __str__(self):
         return self.name
+
+class DailySpecialManager(models.Manager):
+    def upcoming(self):
+        today = datetime.date.today()
+        return self.filter(date_gte=today)
+
 
     @staticmethod
     def get_random_special():
