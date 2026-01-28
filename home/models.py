f1d12aa239ee959f86c1e02b9eff6074c33f0a0d
Vignesh Gunagi <gunagivignesh96@gmail.com>
2026-01-28 18:01:07 +0530

    64776

--- home/models.py
+++ home/models.py
@@ -5,6 +5,9 @@ from django import models
 
 class MenuCategory(models.Model):
     name=models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
